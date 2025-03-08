0e34b8b6c4726c5f8d8b1766feac1843b4913d5f
Jashmin14 <jashmin14.in>
2025-03-08 11:27:21 +0530

    feat: Added font to config file

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "kode-mono": ["var(--font-kode-mono)", "monospace"],
+        "fira-mono": ["var(--font-fira-mono)", "monospace"],
+        "andale-mono": ["Andale Mono", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
